c7c719e5ff248022146bbf2f28756c3244aaa7a5
Reebus-boop <agentolivesoil@gmail.com>
2025-02-01 11:38:46 -0800

    Mecnum code added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+
+
+public class RobotHardware {
+    private LinearOpMode myOpMode = null;
+    public RobotHardware(LinearOpMode opMode)
+    {
+        myOpMode = opMode;
+    }
+
+    public DcMotor fLeft;
+    public DcMotor fRight;
+    public DcMotor bLeft;
+    public DcMotor bRight;
+
+
+    public void init()
+    {
+        fLeft = myOpMode.hardwareMap.get(DcMotor.class, "fLeft");
+        fRight = myOpMode.hardwareMap.get(DcMotor.class, "fRight");
+        bLeft = myOpMode.hardwareMap.get(DcMotor.class, "bLeft");
+        bRight = myOpMode.hardwareMap.get(DcMotor.class, "bRight");
+
+
+        fLeft.setDirection(DcMotor.Direction.FORWARD);
+        fRight.setDirection(DcMotor.Direction.REVERSE);
+        bLeft.setDirection(DcMotor.Direction.FORWARD);
+        bRight.setDirection(DcMotor.Direction.REVERSE);
+
+        fLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        fLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        myOpMode.telemetry.addData("status", "initialized");
+        myOpMode.telemetry.update();
+        myOpMode.waitForStart();
+    }
+
+    public void driveRobot(Gamepad gamepad1){
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = -gamepad1.right_stick_x;
+        double slow = (gamepad1.right_trigger*2) +1;
+
+        double frLeft = y + x + rx;
+        double frRight = y - x - rx  ;
+        double baLeft = y - x + rx;
+        double baRight = y + x - rx;
+
+        setDrivePower(frLeft, frRight, baLeft, baRight, slow);
+
+    }
+
+    //Sets driving speed
+    public void setDrivePower(double v1, double v2, double v3, double v4, double s) {
+        double n = 1.15 * s;
+        fLeft.setPower(v1/n);
+        fRight.setPower(v2/n);
+        bLeft.setPower(v3/n);
+        bRight.setPower(v4/n);
+    }
+}
