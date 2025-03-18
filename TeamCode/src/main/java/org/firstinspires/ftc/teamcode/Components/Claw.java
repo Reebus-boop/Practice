879b9714532bf8d300189c00a740f93c0bbccbcb
Reebus-boop <agentolivesoil@gmail.com>
2025-03-17 19:35:50 -0700

    claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Claw.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.Components;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -6,15 +7,17 @@ public class Claw implements Component{
 
     private LinearOpMode myOpMode;
     private Servo clawServo;     // Servo to open/close the claw
-
-    private final double CLAW_OPEN_POSITION = 0;   // Adjust as needed for your claw design
-    private final double CLAW_CLOSE_POSITION = 0.02;  // Adjust as needed for your claw design
+    private DcMotor Wrist;
+    private final double CLAW_OPEN_POSITION = 0.98;   // Adjust as needed for your claw design
+    private final double CLAW_CLOSE_POSITION = 1;  // Adjust as needed for your claw design
 
     @Override
     public void init(RobotHardware robotHardware) {
         myOpMode = robotHardware.myOpMode;
         clawServo = myOpMode.hardwareMap.get(Servo.class, "clawServo");
+        Wrist = myOpMode.hardwareMap.get(DcMotor.class, "Wrist")
         clawServo.setPosition(CLAW_CLOSE_POSITION);
+        Wrist.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void clawOpen() {
@@ -31,4 +34,7 @@ public class Claw implements Component{
         return clawServo.getPosition();
     }
 
+    public void DcMotor getWrist() {
+        return Wrist;
+    }
 }
